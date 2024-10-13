a1f38145a56773f6e70b675e3d833ec5992ba0a5
tibaas <igorox2@hotmail.com>
2024-10-13 02:35:15 -0300

    adding login page and stuff

--- src/components/LoginPage/LogScreen.tsx
+++ src/components/LoginPage/LogScreen.tsx
@@ -1,7 +1,39 @@
+import { GlobalLoginPageContainer, LoginContainer, InputsContainer } from "./styles";
+import LoginPage from '../../assets/login-page-logo.svg'
+import { Lock, UserCircle } from "phosphor-react";
 export function LogScreen() {
     return (
-        <div>
-            <h1>Tela de login!</h1>
-        </div>
+        <GlobalLoginPageContainer>
+            <img src={LoginPage} alt="" /> 
+            <LoginContainer>
+                <h1>Login</h1>
+                <InputsContainer>
+                    <label>
+                    <span>Usuário</span>
+                    <div>
+
+                        <UserCircle size={23} />
+                    </div>
+                        <input   type="text" />
+                        
+                    </label>
+
+                    <label>
+                    <span>Senha</span>
+                        <input type="password" />
+                    <div>
+                        
+                        <Lock size={23} />
+                    </div>
+                    </label>
+
+                    <button type="submit">
+                        Conectar
+                    </button>
+
+                    
+                </InputsContainer>
+            </LoginContainer>         
+        </GlobalLoginPageContainer>
     )
 }
---
+++ src/components/LoginPage/styles.ts
@@ -0,0 +1,111 @@
+import styled from "styled-components";
+
+export const GlobalLoginPageContainer = styled.div`
+
+width: 100%;
+height: 100vh;
+background: rgba(238, 17, 34, 0.9333333333);
+
+display: flex;
+align-items: center;
+justify-content: center;
+
+img {
+    height: 620px;
+    width: 620px;
+    
+}
+
+`
+
+export const LoginContainer = styled.div`
+position: absolute;
+margin-right: 65px;
+border-radius: 40px;
+height: 595px;
+width: 275px;
+background: #e0e0e0;
+box-shadow: 1px 1px 10px 7px gray;
+
+font-family: 'Ubuntu', sans-serif;
+
+display: flex;
+align-items: center;
+justify-content: center;
+flex-direction: column;
+gap: 5rem;
+
+h1 {
+    
+}
+
+`
+
+export const InputsContainer = styled.div`
+
+display: flex;
+align-items: center;
+flex-direction: column;
+gap: 1rem;
+
+span {
+    margin-bottom: 10px;
+    font-size: 17px;
+    
+}
+
+input {
+    height: 2.7rem;
+    width: 13rem;
+    border: 0;
+    border-radius: 10px;
+    outline: none;
+    text-align: justify;
+    text-indent: 15%;
+    font-size: 0.875rem;
+
+    &:focus{ 
+        border-bottom: 3px solid #000;
+        transition: border-color 0.1s;
+    }
+}
+
+
+button {
+    margin-top: 1rem;
+    height: 2.7rem;
+    width: 13rem;
+    border: 0;
+    border-radius: 10px;
+    color: red;
+    outline: none;
+    font-weight: bold;
+    font-size: 16px;
+    &:hover {
+        background: #e12;
+        cursor: pointer;
+        box-shadow: 1px 1px 1px 2px #000;
+        color: white;
+    }
+}
+
+label { 
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+
+    div {
+        position: absolute;
+        margin-right: 11rem;
+        margin-top: 1.8rem;
+    }
+
+
+
+
+
+
+}
+
+`
